cec9a2c8414b534aea3572a1427d1c33d45939ef
Anar <anar.abdullazada@ufaz.az>
2024-06-14 16:04:50 +0400

    ms

--- index.php
+++ index.php
@@ -1879,7 +1879,7 @@
             </a>
           </div><!-- menu-toggle -->
         </div><!-- column -->
-        <div class="column center">
+        <div class="column center"> 
           <div class="site-brand">
             <a href="#" title="Machic &#8211; Electronics Store eCommerce Theme">
               <img src="./img/n-logo.png"
