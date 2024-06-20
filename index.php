8e84b57cae378fbe2ecd512881e76edf228d5f0d
Anar <anar.abdullazada@ufaz.az>
2024-06-20 13:49:35 +0400

    mobile search

--- index.php
+++ index.php
@@ -1723,29 +1723,7 @@
             <div class="column align-center right">
               <div class="header-form site-search">
                 <span>What are you looking for ?</span>
-                <form action="#" class="search-form" role="search" method="get"
-                  id="searchform">
-                  <div class="input-group">
-                    <div class="input-search-addon"><select class="form-select custom-width" name="product_cat"
-                        id="categories">
-                        <option class="select-value" value="" selected="selected">All</option>
-                        <option value="apple">Apple</option>
-                        <option value="camera-photo">Camera &amp; Photo</option>
-                        <option value="cell-phones">Cell Phones</option>
-                        <option value="computers-accessories">Computers &amp; Accessories</option>
-                        <option value="headphones">Headphones</option>
-                        <option value="smartwatches">Smartwatches</option>
-                        <option value="sports-outdoors">Sports &amp; Outdoors</option>
-                        <option value="television-video">Television &amp; Video</option>
-                        <option value="video-games">Video Games</option>
-                      </select></div><!-- input-search-addon -->
-                    <div class="input-search-field"><i class="klbth-icon-search"></i><input type="search" value=""
-                        class="form-control" name="s" placeholder="Search your favorite product..." autocomplete="off">
-                    </div><!-- input-search-field -->
-                    <div class="input-search-button"><button class="btn btn-primary" type="submit">Search</button></div>
-                    <!-- input-search-button -->
-                  </div><!-- input-group --><input type="hidden" name="post_type" value="product" />
-                </form>
+                <?php include("search-bar.php") ?>
               </div><!-- site-search -->
             </div><!-- column -->
 
@@ -1815,6 +1793,11 @@
           <?php include("navbar-addons.php") ?>
         </div><!-- column -->
       </div><!-- header-wrapper -->
+      <div class="header-form site-search" style="display: grid;">
+        <div style="justify-self: center; width: calc(100% - 112px);">
+          <?php include("search-bar.php") ?>
+        </div>
+      </div>
     </div><!-- header-mobile -->
   </header><!-- site-header -->
 
