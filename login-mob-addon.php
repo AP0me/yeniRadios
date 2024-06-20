5967712c1a50ad599a15b2291dc66f1f369bf59b
Anar <anar.abdullazada@ufaz.az>
2024-06-20 14:50:13 +0400

    addons closer

--- login-mob-addon.php
+++
@@ -1,5 +0,0 @@
-<div class="header-addons login-button" style="color: white">
-  <a href="#my-account/">
-    <div class="header-addons-icon"><i class="klbth-icon-user-1"></i></div>
-  </a>
-</div><!-- header-addons -->
