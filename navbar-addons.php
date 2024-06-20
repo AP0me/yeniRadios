67c9ba6836b62ecdd94dd875764d0c0331a6d438
Anar <anar.abdullazada@ufaz.az>
2024-06-20 13:57:02 +0400

    ms

--- navbar-addons.php
+++ navbar-addons.php
@@ -4,8 +4,6 @@
     <a href="#my-account/">
       <div class="header-addons-icon"><i class="klbth-icon-user-1"></i></div>
       <div class="header-addons-text">
-        <div class="sub-text">Sign In</div>
-        <div class="primary-text">Account</div>
       </div><!-- header-addons-text -->
     </a>
   </div><!-- header-addons -->
