a3b8aabb56c83df91438dbaacbfb3254892afcae
Anar <anar.abdullazada@ufaz.az>
2024-06-20 13:26:13 +0400

    mobile navbar add login and heart

--- index.php
+++ index.php
@@ -1503,81 +1503,7 @@
               </nav><!-- site-menu -->
             </div><!-- column -->
 
-            <div class="column align-center right">
-
-              <div class="header-addons login-button">
-                <a href="#my-account/">
-                  <div class="header-addons-icon"><i class="klbth-icon-user-1"></i></div>
-                  <div class="header-addons-text">
-                    <div class="sub-text">Sign In</div>
-                    <div class="primary-text">Account</div>
-                  </div><!-- header-addons-text -->
-                </a>
-              </div><!-- header-addons -->
-
-
-              <div class="header-addons wishlist-button">
-                <a href="#wishlist/">
-                  <div class="header-addons-icon">
-                    <i class="klbth-icon-heart"></i>
-                    <div class="button-count klbwl-wishlist-count">0</div>
-                  </div><!-- header-addons-icon -->
-                </a>
-              </div><!-- header-addons -->
-
-
-              <div class="header-addons cart-button">
-                <a href="#cart/">
-                  <div class="header-addons-icon">
-                    <i class="klbth-icon-simple-cart"></i>
-                    <div class="button-count cart-count">0</div>
-                  </div><!-- header-addons-icon -->
-                  <div class="header-addons-text hide-mobile">
-                    <div class="sub-text">Total</div>
-                    <div class="primary-text cart-subtotal"><span class="woocommerce-Price-amount amount"><bdi><span
-                            class="woocommerce-Price-currencySymbol">&#36;</span>0.00</bdi></span></div>
-                  </div><!-- header-addons-text -->
-                </a>
-                <div class="cart-dropdown hide">
-                  <div class="cart-dropdown-wrapper">
-                    <div class="fl-mini-cart-content">
-
-
-
-                      <div class="cart-empty">
-                        <div class="empty-icon">
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                            <path d="M460 504H52c-24 0-44-20-44-44V138.4h496v320.8c0 24.8-20 44.8-44 44.8z"
-                              fill="#2769FF" />
-                            <path d="M52 472c-6.4 0-12-5.6-12-12V170.4h432v288.8c0 6.4-5.6 12-12 12H52v.8z"
-                              fill="#fff" />
-                            <path fill="#e1e6e9" d="M18.4 8h462.4v130.4H18.4z" />
-                            <g fill="#fff">
-                              <path d="M504 138.4h-62.4v-64zM480.8 114.4V8l-39.2 66.4zM8 138.4h62.4v-64z" />
-                              <path d="M18.4 8v120l52-53.6z" />
-                            </g>
-                            <g fill="#193651">
-                              <path
-                                d="M512 138.4c0-1.6-.8-4-2.4-5.6l-20.8-21.6V8c0-4.8-4-8-8-8H18.4c-4 0-8 4-8 8v116.8l-8 8.8c-1.6.8-2.4 3.2-2.4 4.8v320.8C0 488.8 23.2 512 52 512h407.2c28.8 0 52-23.2 52-52V138.4h.8zm-60-65.6l20.8-36v57.6L452 72.8zm20.8 44.8v12.8h-23.2V93.6l23.2 24zM467.2 16l-32 53.6c-.8.8-.8 3.2-.8 4v56.8h-356v-56c0-1.6-.8-3.2-1.6-4.8L35.2 16h432zM26.4 31.2l32.8 42.4L26.4 108V31.2zm36 62.4v36.8h-36l36-36.8zM496 460c0 20-16 36-36 36H52c-20 0-36-16-36-36V146.4h480V460z" />
-                              <path
-                                d="M372.8 429.6h16v16h-16zM428.8 429.6h16v16h-16zM67.2 429.6h16v16h-16zM123.2 429.6h16v16h-16zM140.8 264.8c5.6 89.6 55.2 160 115.2 160s108.8-70.4 115.2-160c5.6-2.4 9.6-8.8 9.6-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c0 6.4 3.2 12 8.8 15.2-5.6 81.6-48 144.8-99.2 144.8s-93.6-63.2-99.2-144.8c4.8-3.2 8.8-8.8 8.8-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c1.6 6.4 5.6 12 11.2 15.2z" />
-                            </g>
-                          </svg>
-                        </div><!-- empty-icon -->
-                        <div class="empty-text">No products in the cart.</div>
-                      </div><!-- cart-empty -->
-
-
-                    </div>
-
-                    <div class="cart-noticy">
-                      Free Shipping on All Orders Over $750 </div><!-- cart-noticy -->
-                  </div><!-- cart-dropdown-wrapper -->
-                </div><!-- cart-dropdown -->
-              </div><!-- header-addons -->
-
-
-            </div><!-- column -->
+            <?php include("navbar-addons.php") ?>
 
           </div><!-- header-wrapper -->
         </div><!-- container -->
@@ -1869,18 +1795,16 @@
     </div><!-- header-mobile-switcher -->
 
     <div class="header-mobile hide-desktop">
-      <div class="header-wrapper">
+      <div class="header-wrapper" style="gap: 20px;">
         <div class="column left">
-          <div class="header-addons menu-toggle">
-            <a href="#">
-              <div class="header-addons-icon">
-                <i class="klbth-icon-menu"></i>
-              </div><!-- header-addons-icon -->
-            </a>
-          </div><!-- menu-toggle -->
+          <a href="#">
+            <div class="header-addons-icon">
+              <i class="klbth-icon-menu"></i>
+            </div><!-- header-addons-icon -->
+          </a>
         </div><!-- column -->
         <div class="column center mobile-logo" style="justify-content: start">
-          <div class="site-brand">
+        <div class="site-brand">
             <a href="#" title="Machic &#8211; Electronics Store eCommerce Theme">
               <img src="./img/n-logo2.png"
                 alt="Machic &#8211; Electronics Store eCommerce Theme">
@@ -1888,55 +1812,7 @@
           </div><!-- site-brand -->
         </div><!-- column -->
         <div class="column right">
-          <div class="header-addons cart-button">
-            <a href="#cart/">
-              <div class="header-addons-icon">
-                <i class="klbth-icon-simple-cart"></i>
-                <div class="button-count cart-count">0</div>
-              </div><!-- header-addons-icon -->
-              <div class="header-addons-text hide-mobile">
-                <div class="sub-text">Total</div>
-                <div class="primary-text cart-subtotal"><span class="woocommerce-Price-amount amount"><bdi><span
-                        class="woocommerce-Price-currencySymbol">&#36;</span>0.00</bdi></span></div>
-              </div><!-- header-addons-text -->
-            </a>
-            <div class="cart-dropdown hide">
-              <div class="cart-dropdown-wrapper">
-                <div class="fl-mini-cart-content">
-
-
-
-                  <div class="cart-empty">
-                    <div class="empty-icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M460 504H52c-24 0-44-20-44-44V138.4h496v320.8c0 24.8-20 44.8-44 44.8z"
-                          fill="#2769FF" />
-                        <path d="M52 472c-6.4 0-12-5.6-12-12V170.4h432v288.8c0 6.4-5.6 12-12 12H52v.8z" fill="#fff" />
-                        <path fill="#e1e6e9" d="M18.4 8h462.4v130.4H18.4z" />
-                        <g fill="#fff">
-                          <path d="M504 138.4h-62.4v-64zM480.8 114.4V8l-39.2 66.4zM8 138.4h62.4v-64z" />
-                          <path d="M18.4 8v120l52-53.6z" />
-                        </g>
-                        <g fill="#193651">
-                          <path
-                            d="M512 138.4c0-1.6-.8-4-2.4-5.6l-20.8-21.6V8c0-4.8-4-8-8-8H18.4c-4 0-8 4-8 8v116.8l-8 8.8c-1.6.8-2.4 3.2-2.4 4.8v320.8C0 488.8 23.2 512 52 512h407.2c28.8 0 52-23.2 52-52V138.4h.8zm-60-65.6l20.8-36v57.6L452 72.8zm20.8 44.8v12.8h-23.2V93.6l23.2 24zM467.2 16l-32 53.6c-.8.8-.8 3.2-.8 4v56.8h-356v-56c0-1.6-.8-3.2-1.6-4.8L35.2 16h432zM26.4 31.2l32.8 42.4L26.4 108V31.2zm36 62.4v36.8h-36l36-36.8zM496 460c0 20-16 36-36 36H52c-20 0-36-16-36-36V146.4h480V460z" />
-                          <path
-                            d="M372.8 429.6h16v16h-16zM428.8 429.6h16v16h-16zM67.2 429.6h16v16h-16zM123.2 429.6h16v16h-16zM140.8 264.8c5.6 89.6 55.2 160 115.2 160s108.8-70.4 115.2-160c5.6-2.4 9.6-8.8 9.6-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c0 6.4 3.2 12 8.8 15.2-5.6 81.6-48 144.8-99.2 144.8s-93.6-63.2-99.2-144.8c4.8-3.2 8.8-8.8 8.8-15.2 0-9.6-8-17.6-17.6-17.6s-17.6 8-17.6 17.6c1.6 6.4 5.6 12 11.2 15.2z" />
-                        </g>
-                      </svg>
-                    </div><!-- empty-icon -->
-                    <div class="empty-text">No products in the cart.</div>
-                  </div><!-- cart-empty -->
-
-
-                </div>
-
-                <div class="cart-noticy">
-                  Free Shipping on All Orders Over $750 </div><!-- cart-noticy -->
-              </div><!-- cart-dropdown-wrapper -->
-            </div><!-- cart-dropdown -->
-          </div><!-- header-addons -->
-
+          <?php include("navbar-addons.php") ?>
         </div><!-- column -->
       </div><!-- header-wrapper -->
     </div><!-- header-mobile -->
