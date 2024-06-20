7bd82ed8f0b94300634ba73fdf4f96ae41e62455
Anar <anar.abdullazada@ufaz.az>
2024-06-20 14:04:47 +0400

    s

--- navbar-addons.php
+++ navbar-addons.php
@@ -1,12 +1,14 @@
 <div class="column align-center right">
 
-  <div class="header-addons login-button" style="color: white">
-    <a href="#my-account/">
-      <div class="header-addons-icon"><i class="klbth-icon-user-1"></i></div>
-      <div class="header-addons-text">
-      </div><!-- header-addons-text -->
-    </a>
-  </div><!-- header-addons -->
+    <div class="header-addons login-button" style="color: white">
+      <a href="#my-account/">
+        <div class="header-addons-icon"><i class="klbth-icon-user-1"></i></div>
+        <div class="header-addons-text">
+          <div class="sub-text">Sign In</div>
+          <div class="primary-text">Account</div>
+        </div><!-- header-addons-text -->
+      </a>
+    </div><!-- header-addons -->
 
 
   <div class="header-addons wishlist-button">
